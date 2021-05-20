db8ab88d393572806414d8ab48d5584946436932
Gavin <gavinbrrtt@gmail.com>
2021-05-19 23:34:33 -0700

    Updated example transaction size to 64 bytes.

--- example.py
+++ example.py
@@ -3,8 +3,7 @@ from os import urandom
 from hashregate import MerkleTree
 
 def run_tree():
-	tx = urandom(64)
-	txs = [tx] * 4096
+	txs = [urandom(64) for i in range(4096)]
 	start = time()
 	mt = MerkleTree(txs)
 	print(f'{mt.tree[0].hex()}\nRoot hash generated in {time() - start} seconds')
